90aa31e9a09eb3693a4d2ee7d0c6f97b9b5acaba
soledadrosario <solerosario0225@gmail.com>
2021-03-11 21:29:52 -0400

    Ultimo commit con add correcto

---
+++ src/debbuging-node/index.js
@@ -0,0 +1,31 @@
+/* Algoritmo de ordenamiento  al Burbuja 0(n^2)*/
+
+
+const ordenaBurbuja = (lista) => {
+
+
+    let longitudLista, indexOrder, IndexChange, auxChange;
+    longitudLista = lista.length;
+
+    // console.info(longitudLista); // Mostramos por consola la cantidad de elementos en la lista 
+
+    for (indexOrder = 1; indexOrder <longitudLista; - indexOrder++) {
+        //For animado.
+        for (indexChange = 0; indexChange < (longitudLista - indexOrder); indexChange++) {   
+           // Intercambiamos los valores si el anterio es mayor al siguiente.
+             if (lista[IndexChange] > lista[IndexChange + 1]) {
+                auxChange = lista[IndexChange];
+                lista[indexChange] > lista[IndexChange + 1];
+                lista[indexChange + 1] = auxChange;
+            }
+        }
+    }
+    
+    return lista;
+}
+
+let listaDesordenada = [6, 5, 3, 1, 8, 7, 2, 4];
+console.warn(listaDesordenada); //Mostramos, por consola, la lista desordenada
+
+const listaOrdenada = ordenaBurbuja(listaDesordenada);
+console.log(listaOrdenada); // Mostramos, por consola, la lista ya ordenada
